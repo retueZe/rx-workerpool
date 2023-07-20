3f90259d03c4fd8bebc5a32f666aaca6ae1740af
retueZe <84510709+retueZe@users.noreply.github.com>
2023-07-20 13:18:20 +0300

    Add since's

--- src/abstraction.ts
+++ src/abstraction.ts
@@ -1,20 +1,31 @@
+/** @since v1.0.0 */
 export interface IWorkerPoolKey extends IWorkerPool {
+    /** @since v1.0.0 */
     readonly pool: IWorkerPool
+    /** @since v1.0.0 */
     readonly isAborted: boolean
+    /** @since v1.0.0 */
     minWorkerCount: number
+    /** @since v1.0.0 */
     maxWorkerCount: number | null
 
+    /** @since v1.0.0 */
     abort(): Promise<void>
+    /** @since v1.0.0 */
     setDesiredWorkerType(type: WorkerType | null): this
 }
+/** @since v1.0.0 */
 export interface IWorkerPool {
+    /** @since v1.0.0 */
     readonly isAbortRequested: boolean
 
+    /** @since v1.0.0 */
     queue<C extends (...args: any[]) => any>(
         callback: C,
         ...args: Parameters<C>
     ): Promise<ReturnType<C>>
 }
+/** @since v1.0.0 */
 export type WorkerType =
     | 'web-worker'
     | 'worker-thread'
