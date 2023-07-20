5f8b078f7fc4a6058cee3aed14a0f9862ca18997
retueZe <84510709+retueZe@users.noreply.github.com>
2023-07-20 20:45:43 +0300

    Add support for inline lambda functions

--- src/serialization.ts
+++ src/serialization.ts
@@ -1,6 +1,8 @@
 const FUNCTION_PATTERN = /^(?:function)[^(]*\((?<args>[^)]*)\)\s*{(?<code>.*?)}$/gs
 const LAMBDA_PATTERN = /^\((?<args>[^)]*)\)\s*=>\s*{(?<code>.*)}$/gs
+const INLINE_LAMBDA_PATTERN = /^\((?<args>[^)]*)\)\s*=>\s*(?<result>[^{].*)$/gs
 
+// TODO: add support for `async` functions
 /**
  * In current implementation: `callback.toString()`.
  *
@@ -17,11 +19,14 @@ export function serializeFunction(callback: (...args: any[]) => any): string {
  * @since v1.0.0
  */
 export function deserializeFunction(input: string): (...args: any[]) => any {
-    const match = FUNCTION_PATTERN.exec(input) ?? LAMBDA_PATTERN.exec(input)
+    const match = FUNCTION_PATTERN.exec(input)
+        ?? LAMBDA_PATTERN.exec(input)
+        ?? INLINE_LAMBDA_PATTERN.exec(input)
 
     if (match === null) throw new Error('Bad input.')
 
-    const {args: _args, code} = match.groups!
+    const {args: _args, code: _code, result} = match.groups!
+    const code = _code ?? `return ${result}`
     const args = _args
         .split(',')
         .map(arg => arg.trim())
